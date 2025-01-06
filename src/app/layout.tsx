d03943a19900db01a0857c6404809b83a5dbef78
Alaric Li <alaricli@outlook.com>
2025-01-06 12:28:27 -0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/navigation/navbar/Navbar";
 import Footer from "./components/navigation/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "ptcgpocket.net",
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={`antialiased min-h-screen flex flex-col`}>
         <Navbar />
         <main className="flex-grow">{children}</main>
